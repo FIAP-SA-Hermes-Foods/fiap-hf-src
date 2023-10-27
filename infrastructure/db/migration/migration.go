b5a83b459dfa32581ab0ee33484b6743636dc215
MarcosP <marcos.vp.dev88@gmail.com>
2023-10-26 22:27:45 -0300

    feat(001): update migration code to run codes recursively and integration with windows

--- infrastructure/db/migration/migration.go
+++ infrastructure/db/migration/migration.go
@@ -1,49 +1,104 @@
 package migration
 
 import (
+	"bytes"
 	"context"
-	"database/sql"
-	"fmt"
+	"errors"
+	"fiap-hf-src/infrastructure/db/postgres"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
 
-func DBMigration() error {
+var commands = map[string]*exec.Cmd{
+	"pwd-windows": exec.Command("cmd", "/C", "echo", "%cd%"),
+	"pwd":         exec.Command("bash", "-c", "pwd"),
+}
+
+type MigrationDB interface {
+	Migrate() error
+}
+
+type migrationDB struct {
+	DB postgres.PostgresDB
+}
+
+func NewMigration(db postgres.PostgresDB) MigrationDB {
+	return &migrationDB{DB: db}
+
+}
+
+func (m migrationDB) Migrate() error {
 	ctx := context.Background()
 
-	url := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+	if err := m.DB.Connect(); err != nil {
+		return err
+	}
 
-	db, err := sql.Open("postgres", url)
+	defer m.DB.Close()
+
+	pwdOut, err := runCommandLinuxDarwinOrWindows("pwd")
 
 	if err != nil {
 		return err
 	}
 
-	conn, err := db.Conn(ctx)
+	pwdOutFormatted := formatCommandOutputToStr(pwdOut)
 
+	path := filepath.Join(pwdOutFormatted, "infrastructure", "db", "DML")
+
+	// for unity tests
+	if strings.Contains(path, "/infrastructure/db/migration/infrastructure/db/DML") {
+		path = strings.ReplaceAll(path, "/db/migration/infrastructure/db/DML", "/db/DML/")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	list, err := file.Readdir(-1)
 	if err != nil {
 		return err
 	}
 
-	defer conn.Close()
+	for _, f := range list {
 
-	// TODO: Run this with a PWD command checking by OS system
-	q, err := os.ReadFile("./infrastructure/db/scripts/001_init_db.sql")
+		q, err := os.ReadFile(filepath.Join(path, f.Name()))
 
-	if err != nil {
-		return err
-	}
+		if err != nil {
+			return err
+		}
 
-	if _, err := conn.ExecContext(ctx, string(q)); err != nil {
-		return err
+		if _, err := m.DB.ExecContext(ctx, string(q)); err != nil {
+			return err
+		}
 	}
 
 	return nil
+}
+
+func runCommandLinuxDarwinOrWindows(cmdName string) ([]byte, error) {
+	if strings.EqualFold("windows", runtime.GOOS) {
+		cmdName = cmdName + "-windows"
+	}
+
+	if _, ok := commands[cmdName]; ok {
+		return commands[cmdName].Output()
+	}
+
+	return nil, errors.New("command not found")
+}
+
+func formatCommandOutputToStr(cmdOutput []byte) string {
+	cmdOutput = bytes.ReplaceAll(cmdOutput, []byte("\n"), []byte(" "))
+	cmdStr := string(cmdOutput)
+	return strings.TrimSpace(cmdStr)
 }
