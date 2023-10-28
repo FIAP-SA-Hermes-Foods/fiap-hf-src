0e9772a21ae97ffda1282c98db1b3f5f07e753ed
MarcosP <marcos.vp.dev88@gmail.com>
2023-10-28 02:56:53 -0300

    feat(002): updating server to accept the client endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"context"
+	"fiap-hf-src/infrastructure/db/postgres"
+	cRepo "fiap-hf-src/internal/adapters/driven/repository/client"
+	"fiap-hf-src/internal/core/application"
+	"fiap-hf-src/internal/core/service"
+	"fiap-hf-src/internal/core/ui"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	sMux := http.NewServeMux()
-	sMux.HandleFunc("/hermes_foods", server)
-	log.Fatal(http.ListenAndServe(":8080", sMux))
+	router := http.NewServeMux()
+
+	ctx := context.Background()
+
+	db := postgres.NewPostgresDB(
+		ctx,
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+	)
+
+	clientRepo, clientService := cRepo.NewClientRepository(ctx, db), service.NewClientService()
+
+	app := application.NewHermesFoodsApp(clientRepo, clientService)
+	handlersClient := ui.NewHandlerClient(app)
+
+	router.HandleFunc("/hermes_foods", server)
+	router.HandleFunc("/hermes_foods/health", ui.HealthCheck)
+	router.HandleFunc("/hermes_foods/client", handlersClient.Handler)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func server(rw http.ResponseWriter, req *http.Request) {
