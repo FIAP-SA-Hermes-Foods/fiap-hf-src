a91502194ea970808a1837be1067cb48f250ae53
MarcosP <marcos.vp.dev88@gmail.com>
2023-10-29 18:43:02 -0300

    feat(008): update order repository to add new function to get order by id

--- internal/adapters/driven/repository/order/orderRepository_test.go
+++ internal/adapters/driven/repository/order/orderRepository_test.go
@@ -8,6 +8,131 @@ import (
 	"testing"
 )
 
+// go test -v -count=1 -failfast -cover -run ^Test_GetOrderByID$
+func Test_GetOrderByID(t *testing.T) {
+	ctx := context.Background()
+
+	type args struct {
+		id int64
+	}
+
+	tests := []struct {
+		name        string
+		args        args
+		ctx         context.Context
+		WantOutput  *entity.Order
+		mockDB      *mockDb
+		isWantError bool
+	}{
+		{
+			name:       "success",
+			args:       args{},
+			ctx:        ctx,
+			WantOutput: &entity.Order{},
+			mockDB: &mockDb{
+				WantResult: nil,
+				WantRows:   &sql.Rows{},
+				WantErr:    nil,
+			},
+
+			isWantError: false,
+		},
+		{
+			name:       "connection_error",
+			args:       args{},
+			ctx:        ctx,
+			WantOutput: &entity.Order{},
+			mockDB: &mockDb{
+				WantResult: nil,
+				WantRows:   &sql.Rows{},
+				WantErr:    errors.New("errConnect"),
+			},
+
+			isWantError: true,
+		},
+		{
+			name:       "query_error",
+			args:       args{},
+			ctx:        ctx,
+			WantOutput: &entity.Order{},
+			mockDB: &mockDb{
+				WantResult: nil,
+				WantRows:   &sql.Rows{},
+				WantErr:    errors.New("errQuery"),
+			},
+
+			isWantError: true,
+		},
+		{
+			name:       "prepare_stmt_error",
+			args:       args{},
+			ctx:        ctx,
+			WantOutput: &entity.Order{},
+			mockDB: &mockDb{
+				WantResult: nil,
+				WantRows:   &sql.Rows{},
+				WantErr:    errors.New("errPrepareStmt"),
+			},
+
+			isWantError: true,
+		},
+		{
+			name:       "prepare_stmt_error",
+			args:       args{},
+			ctx:        ctx,
+			WantOutput: &entity.Order{},
+			mockDB: &mockDb{
+				WantResult: nil,
+				WantRows:   &sql.Rows{},
+				WantErr:    errors.New("errScan"),
+			},
+
+			isWantError: true,
+		},
+		{
+			name:       "error_scan_stmt",
+			args:       args{},
+			ctx:        nil,
+			WantOutput: &entity.Order{},
+			mockDB: &mockDb{
+				WantResult:   nil,
+				WantRows:     &sql.Rows{},
+				WantErr:      errors.New("errScanStmt"),
+				WantNextRows: false,
+			},
+			isWantError: true,
+		},
+		{
+			name: "error_scan",
+			args: args{},
+			ctx:  nil,
+			mockDB: &mockDb{
+				WantResult:   nil,
+				WantRows:     &sql.Rows{},
+				WantErr:      errors.New("errScan"),
+				WantNextRows: true,
+			},
+			isWantError: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			repo := NewOrderRepository(tc.ctx, tc.mockDB)
+
+			out, err := repo.GetOrderByID(tc.args.id)
+
+			if (!tc.isWantError) && err != nil {
+				t.Errorf("was not suppose to have an error here and %v got", err)
+			}
+
+			if out != nil && (out.MarshalString() != tc.WantOutput.MarshalString()) {
+				t.Errorf("was not suppose to have:\n%s\n and got:\n%s\n", tc.WantOutput.MarshalString(), out.MarshalString())
+			}
+		})
+	}
+}
+
 // go test -v -count=1 -failfast -cover -run ^Test_SaveOrder$
 func Test_SaveOrder(t *testing.T) {
 	ctx := context.Background()
