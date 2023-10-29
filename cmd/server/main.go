d206bf455bdc39dbfd6dae0113c65d391a8b73d3
MarcosP <marcos.vp.dev88@gmail.com>
2023-10-29 03:18:27 -0300

    feat(007): create new saveOrder endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fiap-hf-src/infrastructure/db/postgres"
 	cRepo "fiap-hf-src/internal/adapters/driven/repository/client"
+	oRepo "fiap-hf-src/internal/adapters/driven/repository/order"
 	"fiap-hf-src/internal/core/application"
 	"fiap-hf-src/internal/core/service"
 	"fiap-hf-src/internal/core/ui"
@@ -29,26 +30,15 @@ func main() {
 	defer db.Close()
 
 	clientRepo, clientService := cRepo.NewClientRepository(ctx, db), service.NewClientService(nil)
+	orderRepo, orderService := oRepo.NewOrderRepository(ctx, db), service.NewOrderService(nil)
 
-	app := application.NewHermesFoodsApp(clientRepo, clientService)
+	app := application.NewHermesFoodsApp(clientRepo, orderRepo, clientService, orderService)
 	handlersClient := ui.NewHandlerClient(app)
+	handlersOrder := ui.NewHandlerOrder(app)
 
-	router.HandleFunc("/hermes_foods", server)
 	router.HandleFunc("/hermes_foods/health", ui.HealthCheck)
 	router.HandleFunc("/hermes_foods/client/", handlersClient.Handler)
+	router.HandleFunc("/hermes_foods/order/", handlersOrder.Handler)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func server(rw http.ResponseWriter, req *http.Request) {
-	rw.Header().Add("Content-Type", "application/json")
-
-	if req.Method != http.MethodPost {
-		rw.WriteHeader(http.StatusMethodNotAllowed)
-		rw.Write([]byte(`{"error": "method not allowed"}`))
-		return
-	}
-
-	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte(`{"message": "Let's kill hunger fast!"}`))
 }
