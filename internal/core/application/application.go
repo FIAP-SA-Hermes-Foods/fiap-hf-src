0faf72df346a55b2d687bc9b7a21151d639ee663
MarcosP <marcos.vp.dev88@gmail.com>
2023-10-29 18:43:44 -0300

    feat(008): update application, adding new functions to save in database and fixing the outputs

--- internal/core/application/application.go
+++ internal/core/application/application.go
@@ -15,11 +15,13 @@ import (
 type HermesFoodsApp interface {
 	// Client Methods
 
-	SaveClient(client entity.Client) (*entity.Client, error)
-	GetClientByCPF(cpf string) (*entity.Client, error)
+	SaveClient(client entity.Client) (*entity.OutputClient, error)
+	GetClientByCPF(cpf string) (*entity.OutputClient, error)
+	GetClientByID(id int64) (*entity.OutputClient, error)
 
 	// Order Methods
-	SaveOrder(order entity.Order) (*entity.Order, error)
+	SaveOrder(order entity.Order) (*entity.OutputOrder, error)
+	GetOrderByID(id int64) (*entity.OutputOrder, error)
 }
 
 type hermesFoodsApp struct {
@@ -42,21 +44,59 @@ func NewHermesFoodsApp(ctx context.Context, paymentAPI httpHF.PaymentAPI, client
 	}
 }
 
-func (h hermesFoodsApp) GetClientByCPF(cpf string) (*entity.Client, error) {
-	if err := h.GetClientByCPFService(cpf); err != nil {
+func (h hermesFoodsApp) GetClientByID(id int64) (*entity.OutputClient, error) {
+	if err := h.GetClientByIDService(id); err != nil {
 		return nil, err
 	}
 
-	client, err := h.GetClientByCPFRepository(cpf)
+	c, err := h.GetClientByIDRepository(id)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return client, err
+	if c == nil {
+		return nil, nil
+	}
+
+	out := &entity.OutputClient{
+		ID:        c.ID,
+		Name:      c.Name,
+		CPF:       c.CPF.Value,
+		Email:     c.Email,
+		CreatedAt: c.CreatedAt.Format(),
+	}
+
+	return out, err
 }
 
-func (h hermesFoodsApp) SaveClient(client entity.Client) (*entity.Client, error) {
+func (h hermesFoodsApp) GetClientByCPF(cpf string) (*entity.OutputClient, error) {
+	if err := h.GetClientByCPFService(cpf); err != nil {
+		return nil, err
+	}
+
+	c, err := h.GetClientByCPFRepository(cpf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if c == nil {
+		return nil, nil
+	}
+
+	out := &entity.OutputClient{
+		ID:        c.ID,
+		Name:      c.Name,
+		CPF:       c.CPF.Value,
+		Email:     c.Email,
+		CreatedAt: c.CreatedAt.Format(),
+	}
+
+	return out, err
+}
+
+func (h hermesFoodsApp) SaveClient(client entity.Client) (*entity.OutputClient, error) {
 	clientWithCpf, err := h.GetClientByCPF(client.CPF.Value)
 
 	if err != nil {
@@ -77,16 +117,62 @@ func (h hermesFoodsApp) SaveClient(client entity.Client) (*entity.Client, error)
 		return nil, errors.New("is not possible to save client because it's null")
 	}
 
-	returnCRepo, err := h.SaveClientRepository(*c)
+	cRepo, err := h.SaveClientRepository(*c)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return returnCRepo, nil
+	out := &entity.OutputClient{
+		ID:        cRepo.ID,
+		Name:      cRepo.Name,
+		CPF:       cRepo.CPF.Value,
+		Email:     cRepo.Email,
+		CreatedAt: cRepo.CreatedAt.Format(),
+	}
+
+	return out, nil
 }
 
-func (h hermesFoodsApp) SaveOrder(order entity.Order) (*entity.Order, error) {
+func (h hermesFoodsApp) GetOrderByID(id int64) (*entity.OutputOrder, error) {
+
+	if err := h.orderService.GetOrderByID(id); err != nil {
+		return nil, err
+	}
+
+	o, err := h.GetOrderByIDRepository(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if o == nil {
+		return nil, nil
+	}
+
+	outClient, err := h.GetClientByID(o.ClientID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if outClient == nil {
+		return nil, errors.New("client is null")
+	}
+
+	out := &entity.OutputOrder{
+		ID:               o.ID,
+		Client:           *outClient,
+		VoucherID:        o.VoucherID,
+		Status:           o.Status.Value,
+		VerificationCode: o.VerificationCode.Value,
+		CreatedAt:        o.CreatedAt.Format(),
+	}
+
+	return out, nil
+}
+
+func (h hermesFoodsApp) SaveOrder(order entity.Order) (*entity.OutputOrder, error) {
 	if err := h.GetClientByIDService(order.ClientID); err != nil {
 		return nil, err
 	}
@@ -136,17 +222,37 @@ func (h hermesFoodsApp) SaveOrder(order entity.Order) (*entity.Order, error) {
 		return nil, errors.New("is not possible to save order because it's null")
 	}
 
-	returnORepo, err := h.SaveOrderRepository(order)
+	oRepo, err := h.SaveOrderRepository(order)
+
 	if err != nil {
 		return nil, err
 	}
 
-	return returnORepo, nil
+	outClient := entity.OutputClient{
+		ID:        c.ID,
+		Name:      c.Name,
+		CPF:       c.CPF.Value,
+		Email:     c.Email,
+		CreatedAt: c.CreatedAt.Format(),
+	}
+
+	outOrder := &entity.OutputOrder{
+		ID:               oRepo.ID,
+		Client:           outClient,
+		VoucherID:        oRepo.VoucherID,
+		Status:           oRepo.Status.Value,
+		VerificationCode: oRepo.VerificationCode.Value,
+		CreatedAt:        oRepo.CreatedAt.Format(),
+	}
+
+	return outOrder, nil
 }
 
 func (h hermesFoodsApp) DoPaymentAPI(ctx context.Context, input entity.InputPaymentAPI) (*entity.OutputPaymentAPI, error) {
 	return h.paymentAPI.DoPayment(ctx, input)
 }
+
+// Client implementation Call
 
 func (h hermesFoodsApp) GetClientByCPFService(cpf string) error {
 	return h.clientService.GetClientByCPF(cpf)
@@ -170,6 +276,16 @@ func (h hermesFoodsApp) SaveClientService(client entity.Client) (*entity.Client,
 
 func (h hermesFoodsApp) SaveClientRepository(client entity.Client) (*entity.Client, error) {
 	return h.clientRepo.SaveClient(client)
+}
+
+// Order implementation Call
+
+func (h hermesFoodsApp) GetOrderByIDRepository(id int64) (*entity.Order, error) {
+	return h.orderRepo.GetOrderByID(id)
+}
+
+func (h hermesFoodsApp) GetOrderByIDService(id int64) error {
+	return h.orderService.GetOrderByID(id)
 }
 
 func (h hermesFoodsApp) SaveOrderRepository(order entity.Order) (*entity.Order, error) {
