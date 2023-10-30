2ac7f052830b4c1fac1214dfefb9425ba0af9648
MarcosP <marcos.vp.dev88@gmail.com>
2023-10-30 02:44:15 -0300

    feat(0013): update order's repository

--- internal/adapters/driven/repository/order/orderRepository.go
+++ internal/adapters/driven/repository/order/orderRepository.go
@@ -8,15 +8,17 @@ import (
 )
 
 var (
-	queryGetOrders    = `SELECT * FROM orders`
+	queryGetOrders    = `SELECT * FROM orders ORDER BY id`
 	queryGetOrderByID = `SELECT * FROM orders WHERE id = $1`
 	querySaveOrder    = `INSERT INTO orders (id, status, verification_code, created_at, client_id, voucher_id) VALUES (DEFAULT, $1, $2, now(), $3, $4) RETURNING id, created_at`
+	queryUpdateOrder  = `UPDATE orders SET status = $1, client_id = $2, voucher_id = $3 WHERE id = $4 RETURNING id, created_at`
 )
 
 type OrderRepository interface {
 	SaveOrder(order entity.Order) (*entity.Order, error)
 	GetOrderByID(id int64) (*entity.Order, error)
 	GetOrders() ([]entity.Order, error)
+	UpdateOrderByID(id int64, order entity.Order) (*entity.Order, error)
 }
 
 type orderRepository struct {
@@ -61,6 +63,39 @@ func (o orderRepository) SaveOrder(order entity.Order) (*entity.Order, error) {
 	return outOrder, nil
 }
 
+func (o orderRepository) UpdateOrderByID(id int64, order entity.Order) (*entity.Order, error) {
+	if err := o.Database.Connect(); err != nil {
+		return nil, err
+	}
+
+	defer o.Database.Close()
+
+	if err := o.Database.PrepareStmt(queryUpdateOrder); err != nil {
+		return nil, err
+	}
+
+	defer o.Database.CloseStmt()
+
+	var outOrder = &entity.Order{
+		ClientID:  order.ClientID,
+		VoucherID: order.VoucherID,
+		Status: valueObject.Status{
+			Value: order.Status.Value,
+		},
+		VerificationCode: valueObject.VerificationCode{
+			Value: order.VerificationCode.Value,
+		},
+	}
+
+	o.Database.QueryRow(order.Status.Value, order.ClientID, order.VoucherID, id)
+
+	if err := o.Database.ScanStmt(&outOrder.ID, &outOrder.CreatedAt.Value); err != nil {
+		return nil, err
+	}
+
+	return outOrder, nil
+}
+
 func (o orderRepository) GetOrderByID(id int64) (*entity.Order, error) {
 	if err := o.Database.Connect(); err != nil {
 		return nil, err
@@ -96,7 +131,6 @@ func (o orderRepository) GetOrderByID(id int64) (*entity.Order, error) {
 }
 
 func (o orderRepository) GetOrders() ([]entity.Order, error) {
-
 	if err := o.Database.Connect(); err != nil {
 		return nil, err
 	}
@@ -133,5 +167,4 @@ func (o orderRepository) GetOrders() ([]entity.Order, error) {
 	}
 
 	return orderList, nil
-
 }
