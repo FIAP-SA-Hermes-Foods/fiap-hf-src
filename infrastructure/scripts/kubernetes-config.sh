c9379ca733707f648a469854e705cab9981a25ba
PauloLucas94 <paulo.lucas@rocketmail.com>
2023-12-16 22:07:11 -0300

    improv(070): Fixed kubernetes-config.sh in scripts directory

--- infrastructure/scripts/kubernetes-config.sh
+++ infrastructure/scripts/kubernetes-config.sh
@@ -36,4 +36,4 @@ sed -i s:{{IMAGE_TAG}}:$IMAGE_TAG:g ./infrastructure/kubernetes/deployment/postg
 sed -i s:{{REPOSITORY_SWAGGER_URL}}:$REPOSITORY_SWAGGER_URL:g ./infrastructure/kubernetes/deployment/swagger.yaml;
 sed -i s:{{IMAGE_TAG}}:$IMAGE_TAG:g ./infrastructure/kubernetes/deployment/swagger.yaml;
 
-sed -i s:{{PWD}}:$PWD:g ./infrastructure/kubernetes/volume/postgres;
+sed -i s:{{PWD}}:$PWD:g ./infrastructure/kubernetes/volume/postgres.yaml;
