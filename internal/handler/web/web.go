abb9c9a5f85daefe355c198ebb1bb4e3ad63e317
MarcosP <marcos.vp.dev88@gmail.com>
2024-01-21 17:56:51 -0300

    improv(082): add new config that makes sense for web file and its test cases in web_test.go file

--- internal/handler/web/web.go
+++ internal/handler/web/web.go
@@ -2,12 +2,77 @@ package web
 
 import (
 	"encoding/base64"
+	"errors"
 	l "fiap-hf-src/pkg/logger"
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 )
+
+func router(reqMethod, path string, routesMap map[string]http.HandlerFunc) (http.HandlerFunc, error) {
+	route := ""
+
+	for k := range routesMap {
+		isValidRoute, rr, m := validRoute(k, path, reqMethod)
+		if isValidRoute && m == strings.ToLower(reqMethod) {
+			route = rr
+		}
+	}
+
+	if handler, ok := routesMap[route]; ok {
+		return handler, nil
+	}
+
+	return nil, errors.New("route not found")
+
+}
+
+func validRoute(route, requestRoute, method string) (bool, string, string) {
+
+	requestRoute = strings.ToLower(method) + " " + requestRoute
+
+	isValid := false
+
+	if requestRoute[len(requestRoute)-1] == '/' {
+		requestRoute = requestRoute[:len(requestRoute)-1]
+	}
+
+	routeItems := strings.Split(route, "/")
+	desiredRouteItems := strings.Split(requestRoute, "/")
+	if len(routeItems) != len(desiredRouteItems) {
+		return false, "", ""
+	}
+
+	idParamVal := ""
+
+	for i := 0; i < len(routeItems); i++ {
+		if strings.Contains(routeItems[i], "{") {
+			idParamVal = desiredRouteItems[i]
+		}
+		if routeItems[i] == desiredRouteItems[i] {
+			isValid = true
+			continue
+		}
+		isValid = false
+	}
+
+	if idParamVal != "" {
+		if _, err := strconv.Atoi(idParamVal); err != nil {
+			return false, "", ""
+		} else {
+			isValid = true
+		}
+	}
+
+	var methodReturn string
+	if len(route) > 0 {
+		methodReturn = strings.Split(route, " ")[0]
+	}
+
+	return isValid, route, methodReturn
+}
 
 func tokenValidate(tokenInput string) error {
 	apiHTokenDecode, err := base64.StdEncoding.DecodeString(tokenInput)
@@ -51,4 +116,26 @@ func getID(handlerName, url string) string {
 	}
 
 	return id
+}
+
+func cleanMethods(url *string) {
+	if url == nil {
+		return
+	}
+
+	var urlVal = *url
+
+	if strings.Contains(*url, "get") {
+		urlVal = urlVal[3:]
+	} else if strings.Contains(*url, "post") {
+		urlVal = urlVal[4:]
+	} else if strings.Contains(*url, "patch") {
+		urlVal = urlVal[5:]
+	} else if strings.Contains(*url, "put") {
+		urlVal = urlVal[3:]
+	} else if strings.Contains(*url, "delete") {
+		urlVal = urlVal[6:]
+	}
+
+	*url = urlVal
 }
